10e857ecd1aaeb7e4b7e550be4bc3f967e9e5789
Kayne Ruse <ratstail91@yahoo.com.au>
2014-06-06 23:08:05 +1000

    Added empty managers for CombatData, RoomData and EnemyData
    These are just placeholders for now, but I'll flesh them out later.
    
    I should note that the server's managers all build cleanly.

---
+++ server/enemy_manager.cpp
@@ -0,0 +1,54 @@
+/* Copyright: (c) Kayne Ruse 2014
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ * 
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ * 
+ * 3. This notice may not be removed or altered from any source
+ * distribution.
+*/
+#include "enemy_manager.hpp"
+
+//-------------------------
+//public access methods
+//-------------------------
+
+//TODO
+
+//-------------------------
+//accessors and mutators
+//-------------------------
+
+EnemyData* EnemyManager::GetEnemy(int uid) {
+	std::map<int, EnemyData>::iterator it = enemyMap.find(uid);
+
+	if (it == enemyMap.end()) {
+		return nullptr;
+	}
+
+	return &it->second;
+}
+
+std::map<int, EnemyData>* EnemyManager::GetContainer() {
+	return &enemyMap;
+}
+
+lua_State* EnemyManager::SetLuaState(lua_State* L) {
+	return luaState = L;
+}
+
+lua_State* EnemyManager::GetLuaState() {
+	return luaState;
+}
