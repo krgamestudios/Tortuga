3399053e64e67d313247eecef0a949b121e047fc
Kayne Ruse <ratstail91@yahoo.com.au>
2014-12-16 21:49:50 +1100

    Added an option to skip missing config files

--- common/utilities/config_utility.hpp
+++ common/utilities/config_utility.hpp
@@ -27,9 +27,9 @@
 #include <map>
 #include <string>
 
-class ConfigUtility : public Singleton<ConfigUtility> {
+class ConfigUtility: public Singleton<ConfigUtility> {
 public:
-	void Load(std::string fname, int argc = 0, char* argv[] = nullptr);
+	void Load(std::string fname, bool skipMissingFile = false, int argc = 0, char* argv[] = nullptr);
 
 	//convert to a type
 	std::string& String(std::string);
@@ -47,10 +47,7 @@ private:
 
 	friend Singleton<ConfigUtility>;
 
-	ConfigUtility() = default;
-	~ConfigUtility() = default;
-
-	table_t Read(std::string fname);
+	table_t Read(std::string fname, bool skipMissingFile);
 
 	table_t configMap;
 };
