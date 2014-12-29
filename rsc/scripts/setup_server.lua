57c6f45c213a0a65164fd6ed1f8cf82a941133d0
Kayne Ruse <ratstail91@yahoo.com.au>
2014-12-30 02:19:19 +1100

    Added API placeholder files

--- rsc/scripts/setup_server.lua
+++ rsc/scripts/setup_server.lua
@@ -4,27 +4,6 @@ mapMaker = require "map_maker"
 mapSaver = require "map_saver"
 roomSystem = require "room_system"
 
-local function dumpTable(t)
-	print(t)
-	for k, v in pairs(t) do
-		print("",k, v)
-	end
-end
-
---create the overworld, set it's generator, loader & saver
---[[
-local t = {
-	"overworld.bmp", --tileset name
-	mapSaver.load, --load function
-	mapSaver.save, --save function
-	mapMaker.debugIsland, --create function
-	mapSaver.save --unload function
-}]]
-
-dumpTable(roomSystem)
-dumpTable(roomSystem.RoomManager)
-dumpTable(roomSystem.Room)
-
 --NOTE: room 0 is the first that the client asks for, therefore it must exist
 local overworld, uid = roomSystem.RoomManager.CreateRoom("overworld")
 roomSystem.Room.Initialize(overworld, "overworld.bmp", mapSaver.Load, mapSaver.Save, mapMaker.debugIsland, mapSaver.Save)
