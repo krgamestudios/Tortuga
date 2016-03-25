18f119224a6e129bec8ab89257e9363ed4c72e66
Kayne Ruse <kayneruse@gmail.com>
2016-03-25 21:53:41 +1100

    Updated the copyright in the scripts

--- rsc/scripts/setup_server.lua
+++ rsc/scripts/setup_server.lua
@@ -1,5 +1,5 @@
 --[[
-/* Copyright: (c) Kayne Ruse 2013-2015
+/* Copyright: (c) Kayne Ruse 2013-2016
  * 
  * This software is provided 'as-is', without any express or implied
  * warranty. In no event will the authors be held liable for any damages
